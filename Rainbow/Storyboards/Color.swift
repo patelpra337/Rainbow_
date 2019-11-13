78a75187d39f7d401344f53fed856fd4f2ac40d2
Pravin Patel <patelpra337@gmail.com>
2019-11-12 21:31:56 -0600

    setup initial view

---
+++ Rainbow/Storyboards/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by patelpra on 11/12/19.
+//  Copyright © 2019 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
