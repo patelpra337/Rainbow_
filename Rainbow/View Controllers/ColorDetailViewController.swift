7d02faf33dfb0313068ea2de29dee390861630bf
Pravin Patel <patelpra337@gmail.com>
2019-11-12 22:28:49 -0600

    finish setting up ViewControllers

--- Rainbow/View Controllers/ColorDetailViewController.swift
+++ Rainbow/View Controllers/ColorDetailViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class ColorDetailViewController: UIViewController {
+    
+    var cellColor: MyColor?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        if let cellColor = cellColor {
+            title = cellColor.name
+            view.backgroundColor = cellColor.color
+        }
     }
-    */
-
 }
